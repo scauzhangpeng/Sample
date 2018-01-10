642c7236d4f10fcd249414215caa90404ef72342
scau_zhangpeng <scau_zhangpeng@163.com>
2018-01-10 15:21:50 +0800

    [新增提交] 1.新增：Button在5.0以后的阴影去除

---
+++ app/src/main/java/com/xyz/sample/ButtonStyleActivity.java
@@ -0,0 +1,18 @@
+package com.xyz.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ZP on 2018/1/10.
+ * 去除5.0 之后Button的阴影
+ */
+
+public class ButtonStyleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_button_style);
+    }
+}
