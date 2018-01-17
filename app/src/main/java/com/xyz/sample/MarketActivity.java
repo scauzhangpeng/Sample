0f25e1199376c322fe427d731ac1e5d965544fb1
scau_zhangpeng <scau_zhangpeng@163.com>
2018-01-17 17:01:32 +0800

    1.新增：跳转应用市场工具类

---
+++ app/src/main/java/com/xyz/sample/MarketActivity.java
@@ -0,0 +1,50 @@
+package com.xyz.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.xyz.util.MarketUtil;
+
+/**
+ * Created by ZP on 2018/1/17.
+ */
+
+public class MarketActivity extends BaseActivity {
+
+    private static final String APP_PACKAGE = "com.autonavi.minimap";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_market);
+    }
+
+    public void goToMarket(View view) {
+        MarketUtil.toMarket(this, APP_PACKAGE, null);
+    }
+
+    public void checkMarkets(View view) {
+        MarketUtil.checkMarket(this);
+    }
+
+    public void goToQQDownload(View view) {
+        MarketUtil.toQQDownload(this, APP_PACKAGE);
+    }
+
+    public void goTo360Download(View view) {
+        MarketUtil.to360Download(this, APP_PACKAGE);
+    }
+
+    public void goToWandoujia(View view) {
+        MarketUtil.toWandoujia(this, APP_PACKAGE);
+    }
+
+    public void goToXiaoMi(View view) {
+        MarketUtil.toXiaoMi(this, APP_PACKAGE);
+    }
+
+    public void goToMeizu(View view) {
+        MarketUtil.toMeizu(this, APP_PACKAGE);
+    }
+}
