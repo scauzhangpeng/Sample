8caa0234ba8f393c67e04fe79652840eed51f23f
scau_zhangpeng <scau_zhangpeng@163.com>
2018-03-01 15:00:32 +0800

    【新增开发】 1.新增：闪光灯工具类

--- app/src/main/java/com/xyz/sample/MainActivity.java
+++ app/src/main/java/com/xyz/sample/MainActivity.java
@@ -5,20 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.xyz.util.CameraUtil;
 import com.xyz.util.DeviceUtil;
 import com.xyz.util.IntentUtil;
 import com.xyz.util.PackageUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mEtPackage;
     private EditText mEtLabel;
     private LinearLayout mLlXiaomi;
     private TextView mTvVersion;
+    private Button btn_open_flash;
+    private Button btn_close_flash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
         mEtLabel = (EditText) findViewById(R.id.et_label);
         mLlXiaomi = (LinearLayout) findViewById(R.id.ll_xiaomi);
         mTvVersion = (TextView) findViewById(R.id.tv_version);
+        btn_open_flash = (Button) findViewById(R.id.btn_open_flash);
+        btn_open_flash.setOnClickListener(this);
+        btn_close_flash = (Button) findViewById(R.id.btn_close_flash);
+        btn_close_flash.setOnClickListener(this);
     }
 
     private void initData() {
@@ -57,6 +66,33 @@ public class MainActivity extends AppCompatActivity {
             mEtPackage.setText(this.getPackageName());
             mEtLabel.setText(PackageUtil.getAppName(this));
             mTvVersion.setText("MIUI 版本：" + DeviceUtil.getSysUIVersionCompat(DeviceUtil.ROM.MIUI));
+        }
+    }
+
+    public void openFlash() {
+        boolean b = CameraUtil.getInstance().openFlash(this);
+        if (!b) {
+            Toast.makeText(this, "打开闪关灯失败", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void closeFlash() {
+        boolean b = CameraUtil.getInstance().closeFlash();
+        if (!b) {
+            Toast.makeText(this, "关闭闪关灯失败", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_open_flash:
+                openFlash();
+                break;
+            case R.id.btn_close_flash:
+                closeFlash();
+                break;
         }
     }
 }
---
+++ util/src/main/java/com/xyz/util/CameraUtil.java
@@ -0,0 +1,118 @@
+package com.xyz.util;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+/**
+ * 相机工具类.
+ * <p>打开/关闭闪光灯，兼容性</p>
+ * Created by ZP on 2018/2/28.
+ */
+
+public class CameraUtil {
+
+    private String mId;
+    private CameraManager mCameraManager;
+    private Camera mCamera;
+
+    private CameraUtil() {
+
+    }
+
+    public static CameraUtil getInstance() {
+        return InnerClazz.instance;
+    }
+
+
+    public boolean openFlash(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return openFlashM(context);
+        } else {
+            return openFlashLollipop();
+        }
+    }
+
+    public boolean closeFlash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return closeFlashM();
+        } else {
+            return closeFlashLollipop();
+        }
+    }
+
+
+    private boolean openFlashM(Context context) {
+        mCameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        if (mCameraManager == null) {
+            return false;
+        } else {
+            try {
+                String[] cameraIdList = mCameraManager.getCameraIdList();
+                for (String id : cameraIdList) {
+                    CameraCharacteristics cameraCharacteristics = mCameraManager.getCameraCharacteristics(id);
+                    Boolean available = cameraCharacteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                    /*
+                     * 获取相机面对的方向
+                     * CameraCharacteristics.LENS_FACING_FRONT 前置摄像头
+                     * CameraCharacteristics.LENS_FACING_BACK 后置摄像头
+                     * CameraCharacteristics.LENS_FACING_EXTERNAL 外部的摄像头
+                     */
+                    Integer integer = cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
+
+                    if (available != null && available && integer != null && integer == CameraCharacteristics.LENS_FACING_BACK) {
+                        mCameraManager.setTorchMode(id, true);
+                        mId = id;
+                        return true;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private boolean closeFlashM() {
+        if (mCameraManager != null) {
+            try {
+                mCameraManager.setTorchMode(mId, false);
+                return true;
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private boolean openFlashLollipop() {
+        if (mCamera == null) {
+            mCamera = Camera.open();
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean closeFlashLollipop() {
+        if (mCamera != null) {
+            mCamera.startPreview();
+            mCamera.release();
+            mCamera = null;
+            return true;
+        }
+        return false;
+    }
+
+
+    private static class InnerClazz {
+        private static final CameraUtil instance = new CameraUtil();
+    }
+}
